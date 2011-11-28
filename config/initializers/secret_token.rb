dbba9bd079e53930e8d21a1ce90976ffa465f12c
Kei Inagawa <k.inaga1028@gmail.com>
2011-11-28 23:31:16 +0900

    'inagawa'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inagawa::Application.config.secret_token = '0530f0c09dc52a500bda1fc6b77273fd5fb7a39617e09f911d3042092c6f75130fbf6762cb37ef7cfd697678a9874703b962edc6a87a8cc2c9230f236e1f8aee'
